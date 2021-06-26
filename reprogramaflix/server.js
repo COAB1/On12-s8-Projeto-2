7ba07c23832524bf184a1ea3881d4fab4cb7038f
COAB1 <carol.o.ag@gmail.com>
2021-06-26 13:34:43 -0300

    Adiciona o endpoint principal

--- reprogramaflix/server.js
+++ reprogramaflix/server.js
@@ -1,6 +1,13 @@
+const { response } = require("express");
 const express = require("express"); //importando express
 
 const app = express();
+
+app.get("/", (request, response) => {
+  response.status(200).send({
+    "chave": "olá pessoas, sejam bem vindas"
+  })
+});
 
 app.listen(3000, () => {
   console.log("estou rodando, babier")
